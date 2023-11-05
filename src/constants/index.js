afd5d139cd2f960a28663be982f53cc2393f95b4
fahd ahsyani <fahdahsayni@gmail.com>
2023-11-05 21:25:10 +0100

    let's Go

---
+++ src/constants/index.js
@@ -0,0 +1,53 @@
+import anapec from "../assets/logos/anapec.png";
+import atos from "../assets/logos/atos.png";
+import capgemini from "../assets/logos/capgemini.png";
+import cdg from "../assets/logos/cdg.png";
+import dxc from "../assets/logos/dxc.png";
+import efe from "../assets/logos/efe.png";
+import giz from "../assets/logos/giz.png";
+import laStartupStation from "../assets/logos/laStartupStation.png";
+import mnc from "../assets/logos/mnc.png";
+import technopark from "../assets/logos/technopark.png";
+
+export const partnerLogos = [
+  {
+    name: "Anapec",
+    logo: anapec,
+  },
+  {
+    name: "Atos",
+    logo: atos,
+  },
+  {
+    name: "Capgemini",
+    logo: capgemini,
+  },
+  {
+    name: "CDG",
+    logo: cdg,
+  },
+  {
+    name: "DXC",
+    logo: dxc,
+  },
+  {
+    name: "EFE",
+    logo: efe,
+  },
+  //   {
+  //     name: "GIZ",
+  //     logo: giz,
+  //   },
+  {
+    name: "laStartupStation",
+    logo: laStartupStation,
+  },
+  {
+    name: "MNC",
+    logo: mnc,
+  },
+  {
+    name: "Technopark",
+    logo: technopark,
+  },
+];
